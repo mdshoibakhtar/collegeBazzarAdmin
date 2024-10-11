c5b544ded51e0cd8ce223b8d6a582e2218771ee5
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-10-11 18:39:19 +0530

    today my work

---
+++ src/pages/transaction/bankCash/bankPayment/index.js
@@ -0,0 +1,12 @@
+import BankPayment from "../../../../components/transaction/bankCash/bankPayment/BankPayment"
+
+
+const BankPaymentPage = ({ heading }) => {
+    return (
+        <>
+            <BankPayment heading={heading} />
+        </>
+    )
+}
+
+export default BankPaymentPage
