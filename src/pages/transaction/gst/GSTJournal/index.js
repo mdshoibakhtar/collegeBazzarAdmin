9e9b157eb9206c479aee50367bc256a531b6e0a7
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-10-15 18:49:51 +0530

    today my work

---
+++ src/pages/transaction/gst/GSTJournal/index.js
@@ -0,0 +1,11 @@
+import GSTJournal from "../../../../components/transaction/gst/GSTJournal/GSTJournal"
+
+const GSTJournalPage = () => {
+    return (
+        <>
+            <GSTJournal />
+        </>
+    )
+}
+
+export default GSTJournalPage
