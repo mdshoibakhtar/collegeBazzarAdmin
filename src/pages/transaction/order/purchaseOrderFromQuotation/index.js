bb2f27b478b088ee27934498aceb809d9e6977bd
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-10-17 18:52:39 +0530

    today my work

---
+++ src/pages/transaction/order/purchaseOrderFromQuotation/index.js
@@ -0,0 +1,12 @@
+import PurchaseOrderFromQuotation from "../../../../components/transaction/order/purchaseOrderFromQuotation/PurchaseOrderFromQuotation"
+
+
+const PurchaseOrderFromQuotationPage = () => {
+    return (
+        <>
+            <PurchaseOrderFromQuotation />
+        </>
+    )
+}
+
+export default PurchaseOrderFromQuotationPage
