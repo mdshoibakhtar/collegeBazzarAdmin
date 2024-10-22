a97123305b8887669c213ed5aabaaffa93e0fe79
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-10-22 18:20:53 +0530

    today my work

---
+++ src/pages/transaction/stockJurnal/index.js
@@ -0,0 +1,13 @@
+import StockJurnal from "../../../components/transaction/stockJurnal/StockJurnal"
+
+
+
+const StockJurnerlPage = () => {
+    return (
+        <>
+            <StockJurnal />
+        </>
+    )
+}
+
+export default StockJurnerlPage
